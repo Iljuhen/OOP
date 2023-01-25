82c4bab4bc6e2eebdcda3b7c12310d3ca60dc324
ilja <bucas@yandex.ru>
2023-01-25 21:50:40 +0300

    изм 2

--- sems/sem2/InvestStatic.java
+++ sems/sem2/InvestStatic.java
@@ -1,16 +1,21 @@
 package sems.sem2;
 
 public class InvestStatic {
+//* Создаем класс */
 
-    public static int maxId = 5;
+
+
+    public static int maxId = 0;
 
     private int id;
 
+
+// Создаем конструктор
     public InvestStatic(int id) {
         maxId++;
         this.id = maxId;
     }
-
+// Создаем геттер
     public int getId() {
         return id;
     }
