d2d410b7a32b9a917424973849caf65494eaa495
iljuhen <bucas@yandex.ru>
2023-02-07 04:22:31 +0300

    linuxNew

--- sems/sem4/Weapons/Sword.java
+++ sems/sem4/Weapons/Sword.java
@@ -2,29 +2,30 @@ package sems.sem4.Weapons;
 
 import java.util.Random;
 
-import sems.sem4.Weaponable;
-
-
 public class Sword extends Weapon {
+    public int nextInt(int min, int max) {
+        return 0;
+
+    }
 
     public Sword(int pointOfDamage) {
         super(pointOfDamage);
-        
+
     }
-    
-    @Override 
-    public String toString() { 
-       
-        return String.format("Sword damage = %d", pointOfDamage); 
+
+    @Override
+    public String toString() {
+
+        return String.format("Sword damage = %d", pointOfDamage);
     }
-    
+
     @Override
     public int damage() {
-        return new Random().nextInt(0, pointOfDamage);
+        int min = 0;
+        int max = pointOfDamage;
+        int my_rnd = new Random().nextInt(0, pointOfDamage);
+
+        return my_rnd;
     }
 
-    
-    
 }
-    
-
